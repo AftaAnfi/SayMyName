81ddc16c8740a30391a9fdf540c677d3ab62c459
AftaAnfi <77285400+AftaAnfi@users.noreply.github.com>
2021-01-21 08:12:14 -0700

    Change title, get input and repeat input in a statement

--- SayMyNameProject/SayMyName.vb
+++ SayMyNameProject/SayMyName.vb
@@ -1,6 +1,23 @@
-﻿Module SayMyName
+﻿Option Explicit On
+Option Strict On
+
+'Aftanom Anfilofieff
+'RCET0265
+'Spring 2021
+'Say My Name
+'https://github.com/AftaAnfi/SayMyName.git
+
+Module SayMyName
+    Dim inputstring As String
+
 
     Sub Main()
+        Console.Title = "Say My Name"
+        Console.WriteLine("What is your name?")
+        inputstring = Console.ReadLine()
+        Console.WriteLine($"Hello there {inputstring}.")
+        Console.ReadKey(False)
+
 
     End Sub
 
